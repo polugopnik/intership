d47e23d4df4bb0feb386a55274280828fcb04cd9
polugopnik <roberto2890@bk.ru>
2026-03-17 19:06:02 +0300

    fin

--- bin/1.1.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  int age = 26;
-  double averageGrade = 4.5;
-  String name = 'Денис';
-  bool isStudent = true;
-print ('Привет, $name! Тебе $age лет. Твой средний балл: $averageGrade. Ты студент? ${isStudent ? 'да' : 'нет'}');
-}
