396247ab76d76c966bf803d12119ed2bc77330a0
polugopnik <roberto2890@bk.ru>
2026-03-17 18:57:26 +0300

    Finish 2.3

---
+++ bin/2.3.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main() {
+  List<List<String>> board = [];
+
+  for (int row = 0; row < 8; row++) {
+    List<String> currentRow = [];
+
+    for (int col = 0; col < 8; col++) {
+      if ((row + col) % 2 == 0) {
+        currentRow.add('□');
+      } else {
+        currentRow.add('■');
+      }
+    }
+
+    board.add(currentRow);
+  }
+
+  print('Введите координаты:');
+  String coord = (stdin.readLineSync() ?? '').toUpperCase();
+
+  int col = coord[0].codeUnitAt(0) - 'A'.codeUnitAt(0);
+  int row = int.parse(coord[1]) - 1;
+
+  print('Цвет клетки: ${board[row][col]}');
+}
