4c1fbd80a8903ebaa6fc892e45d0b495a52f2443
polugopnik <roberto2890@bk.ru>
2026-03-20 20:30:13 +0300

    kakoe zhe govno

---
+++ bin/Proverka.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+void main(){
+  while (true) {
+    print('Введите выражение:');
+    String input = stdin.readLineSync()!;
+    if(input == 'exit'){
+      break;
+    }
+    if (!RegExp(r'^[0-9+\-*/\s]+$').hasMatch(input)) {
+      print('Некорректный ввод');
+      continue;
+    }
+    List<String> stroki = input.split(' ');
+    if (stroki.length != 3) {
+      print('Некорректный ввод');
+      continue;
+    }
+    int a =  int.parse(stroki[0]);
+    String oper = stroki[1];
+    int b =  int.parse(stroki[2]);
+    if (oper == '/' && b == 0){
+      print('На ноль делить нельзя');
+      continue;
+
+    }
+
+  }
+}
